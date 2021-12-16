f33dc063add844f613ec3cb1138dcf382fda9a1c
D-B-R <daniel.b.rocha@hotmail.com>
2021-12-15 21:23:55 -0300

    0.1

---
+++ Projetos c#/AprendendoCSharp/1-PProjeto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ola mundo");
+            Console.WriteLine("fim aperta entre");
+            Console.ReadLine();
+        }
+    }
+}
